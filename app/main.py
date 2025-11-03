78cb5c70d521d91b0e87285852277be74bd95765
Andrii Usypiv <anriyusypiv@gmail.com>
2025-11-03 12:25:15 +0200

    Result1

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 class Person:
     people = {}
 
-    def __init__(self, name: str, age: int):
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[self.name] = self
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<Person: {self.name}>"
 
 
@@ -14,18 +14,18 @@ def create_person_list(people: list[dict]) -> list[Person]:
     Person.people.clear()
 
     for person_data in people:
-        Person(person_data['name'], person_data['age'])
+        Person(person_data["name"], person_data["age"])
 
     for person_data in people:
-        person_obj = Person.people[person_data['name']]
+        person_obj = Person.people[person_data["name"]]
 
-        if 'wife' in person_data and person_data['wife'] is not None:
-            wife_name = person_data['wife']
+        if "wife" in person_data and person_data["wife"] is not None:
+            wife_name = person_data["wife"]
             wife_obj = Person.people[wife_name]
             person_obj.wife = wife_obj
 
-        if 'husband' in person_data and person_data['husband'] is not None:
-            husband_name = person_data['husband']
+        if "husband" in person_data and person_data["husband"] is not None:
+            husband_name = person_data["husband"]
             husband_obj = Person.people[husband_name]
             person_obj.husband = husband_obj
 
